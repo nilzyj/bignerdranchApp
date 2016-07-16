f31e2dba9c4ceb6e37013e1bdc276d2565d9940e
nilzyj <nilzyj@163.com>
2016-07-16 09:04:30 +0800

    chapter 14 写入数据库

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -1,6 +1,12 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.bignerdranch.android.criminalintent.database.CrimeBaseHelper;
+import com.bignerdranch.android.criminalintent.database.CrimeDbSchema;
+import com.bignerdranch.android.criminalintent.database.CrimeDbSchema.CrimeTable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +19,9 @@ import java.util.UUID;
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
-    private List<Crime> mCrimes;
+    //private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -23,8 +31,11 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext)
+                .getWritableDatabase();
         //创建List用于保存Crime对象
-        mCrimes = new ArrayList<>();
+//        mCrimes = new ArrayList<>();
         //数组中存入Crime对象
 //        for (int i = 0; i < 100; i++) {
 //            Crime crime = new Crime();
@@ -35,21 +46,43 @@ public class CrimeLab {
     }
 
     public void addCrime(Crime c) {
-        mCrimes.add(c);
+        //mCrimes.add(c);
+        ContentValues values = getContentValues(c);
+
+        mDatabase.insert(CrimeTable.NAME, null, values);
     }
 
     //返回数组列表
     public List<Crime> getCrimes() {
-        return mCrimes;
+        //return mCrimes;
+        return new ArrayList<>();
     }
 
     //返回带有指定ID的Crime对象
     public Crime getCrime(UUID id) {
-        for (Crime crime : mCrimes) {
-            if (crime.getId().equals(id)) {
-                return crime;
-            }
-        }
+//        for (Crime crime : mCrimes) {
+//            if (crime.getId().equals(id)) {
+//                return crime;
+//            }
+//        }
         return null;
+    }
+
+    public void updateCrime(Crime crime) {
+        String uuidString = crime.getId().toString();
+        ContentValues values = getContentValues(crime);
+
+        mDatabase.update(CrimeTable.NAME, values,
+                CrimeTable.Cols.UUID + " = ?",
+                new String[] {uuidString});
+    }
+
+    private static ContentValues getContentValues(Crime crime) {
+        ContentValues values = new ContentValues();
+        values.put(CrimeTable.Cols.UUID,crime.getId().toString());
+        values.put(CrimeTable.Cols.TITLE,crime.getTitle());
+        values.put(CrimeTable.Cols.DATE,crime.getDate().getTime());
+        values.put(CrimeTable.Cols.SOLVED,crime.isSolved() ? 1 : 0);
+        return values;
     }
 }
