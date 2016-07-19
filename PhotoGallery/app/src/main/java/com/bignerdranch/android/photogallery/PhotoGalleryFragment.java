539c755f45a2fd0c9c4df4513c041e53168f84aa
Adam Thompson <adam@serialphotog.com>
2016-07-19 14:26:23 +0800

    网络连接基本代码

---
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,37 @@
+package com.bignerdranch.android.photogallery;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by dim on 2016/7/19.
+ */
+public class PhotoGalleryFragment extends Fragment {
+    private RecyclerView mPhotoRecyclerView;
+
+    public static PhotoGalleryFragment newInstance() {
+        return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+
+        mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_photo_gallery_recycler_view);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        return v;
+    }
+}
