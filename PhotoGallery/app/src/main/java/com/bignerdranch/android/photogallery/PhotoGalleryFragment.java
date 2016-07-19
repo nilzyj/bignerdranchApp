ee78e95a1fa4f18d8ba0ff4f87ade157bc05aa7a
Adam Thompson <adam@serialphotog.com>
2016-07-19 15:05:33 +0800

    从Flickr获取json数据,error

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -6,12 +6,9 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.io.IOException;
 
 /**
  * Created by dim on 2016/7/19.
@@ -45,13 +42,14 @@ public class PhotoGalleryFragment extends Fragment {
     private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... voids) {
-            try {
-                String result = new FlickrFetchr()
-                        .getUrlString("https://www.bignerdranch.com");
-                Log.i(TAG, "Fetched contents of URL: " + result);
-            } catch (IOException ioe) {
-                Log.e(TAG, "Failed to fetch URL: ", ioe);
-            }
+//            try {
+//                String result = new FlickrFetchr()
+//                        .getUrlString("https://www.bignerdranch.com");
+//                Log.i(TAG, "Fetched contents of URL: " + result);
+//            } catch (IOException ioe) {
+//                Log.e(TAG, "Failed to fetch URL: ", ioe);
+//            }
+            new FlickrFetchr().fetchItems();
             return null;
         }
     }
