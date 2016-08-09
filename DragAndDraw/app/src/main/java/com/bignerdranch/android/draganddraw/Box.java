9e9cbf2308a4fbcca81d904b1a19d716f59e4cf3
nilzyj <nilzyj@163.com>
2016-08-09 11:39:18 +0800

    add DragAndDraw

---
+++ DragAndDraw/app/src/main/java/com/bignerdranch/android/draganddraw/Box.java
@@ -0,0 +1,28 @@
+package com.bignerdranch.android.draganddraw;
+
+import android.graphics.PointF;
+
+/**
+ * Created by dim on 2016/8/9.
+ */
+public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
+
+    public Box(PointF origin) {
+        mOrigin = origin;
+        mCurrent = origin;
+    }
+
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+
+    public PointF getOrigin() {
+        return mOrigin;
+    }
+}
